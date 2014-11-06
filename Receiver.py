91502e74cbcf5bb2bbac62a858bdcea05d9861c2
Artur Renault <artur.renault@gmail.com>
2014-11-06 12:19:07 -0500

    Handle packet delays, corruption, and ordering

--- Receiver.py
+++ Receiver.py
@@ -3,80 +3,12 @@ import datetime
 import signal
 import socket
 import sys
-
-
-def unpack_packet(segment):
-    header = bytearray(segment[:20])
-    packet_source_port = int(header[0]) * 256 + int(header[1])
-    packet_dest_port = int(header[2]) * 256 + int(header[3])
-    packet_seqnum = 256 * 256 * 256 * int(header[4]) +\
-                    256 * 256 * int(header[5]) +\
-                    256 * int(header[6]) +\
-                    int(header[7])
-    packet_acknum = 256 * 256 * 256 * int(header[8]) +\
-                    256 * 256 * int(header[9]) +\
-                    256 * int(header[10]) +\
-                    int(header[11])
-    packet_final = int(header[13])
-    packet_window_size = 256 * int(header[14]) + int(header[15])
-    packet_checksum = 256 * int(header[16]) + int(header[17])
-    packet_contents = str(segment[20:])
-    return packet_source_port, packet_dest_port,\
-           packet_seqnum, packet_acknum, packet_final,\
-           packet_window_size, packet_checksum, packet_contents
-
-
-def make_ack(num):
-    header = bytearray(20)
-
-    header[0] = out_port / 256 # first two bytes are source port
-    header[1] = out_port % 256
-
-    header[2] = sender_port / 256 # second two bytes are destination port
-    header[3] = sender_port % 256
-
-    header[4] = num / 256 / 256 / 256 # next four bytes are sequence number
-    header[5] = num / 256 / 256
-    header[6] = num / 256
-    header[7] = num % 256
-
-    header[8] = num / 256 / 256 / 256 # next four bytes are ack number
-    header[9] = num / 256 / 256
-    header[10] = num / 256
-    header[11] = num % 256
-
-    header[12] = 5 * 16  # header size is always 5 words in our case
-                         # multiply by 16 to leave unused bytes blank
-
-    # header[13] is 2 empty bits + the flag field
-    # In the receiver, it is always 16 because the ACK bit is the only
-    # activated one.
-    if final:
-        header[13] = 17
-    else:
-        header[13] = 16
-
-    header[14] = window_size / 256
-    header[15] = window_size % 256
-
-    header[16] = checksum / 256
-    header[17] = checksum % 256
-
-    header[18] = 0
-    header[19] = 0
-
-    return header
-
-
-def shutdown(signum, frame):
-    sock.close()
-    ack_sock.close()
-    exit(0)
-
+import util
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGINT, util.shutdown)
 
+    # Set command line args
     try:
         filename = sys.argv[1]
         listen_port = int(sys.argv[2])
@@ -87,47 +19,81 @@ if __name__ == '__main__':
     except IndexError, TypeError:
         exit("usage: ./receiver.py <filename> <listening_port> <sender_IP> <sender_port> <log_filename>")
 
+    # Establish initial sockets
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("", listen_port))
+        # UDP socket for receiving file
+        recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        recv_sock.bind(("", listen_port))
 
+        # TCP socket for sending ACKs
         ack_sock = socket.socket()
         ack_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     except socket.error:
         exit("Error creating socket.")
 
+    # Open files for logging and writing
     recv_file = open(filename, 'w')
     if log_filename == "stdout":
         log_file = sys.stdout
     else:
-        log_file = open(log_filename , 'w')
+        log_file = open(log_filename, 'w')
 
-    packet, addr = sock.recvfrom(576)
-    source_port, dest_port, seqnum, acknum,\
-        final, window_size, checksum, contents = unpack_packet(packet)
+    next_acknum = 0
+
+    # Receive first packet
+    packet, addr = recv_sock.recvfrom(576)
+    source_port, dest_port, seqnum, acknum, header_length, \
+        ack, final, window_size, contents = util.unpack(packet)
+
+    checksum = util.get_checksum(packet)
+    packet_valid = checksum == 0 and next_acknum == acknum
+
+    if packet_valid:
+        recv_file.write(contents)
+        next_acknum += 1
 
     recv_file.write(contents)
-    log = str(datetime.datetime.now()) + " " + str(source_port) + " " + str(dest_port) + " " + str(seqnum) + " " + str(acknum)
+    log = str(datetime.datetime.now()) + " " + str(source_port) + " " + str(dest_port) + " " + str(seqnum) + " " + str(
+        acknum)
     log_file.write(log + "\n")
 
+    # Establish ack socket connection
     ack_sock.connect((sender_ip, sender_port))
     out_port = ack_sock.getsockname()[1]
-    ack_segment = make_ack(seqnum)
+    ack_segment = util.make_packet(out_port, sender_port,
+                                   seqnum, acknum, True,
+                                   False, 1, "")
     ack_sock.send(ack_segment)
+
+    # At this point, we are connected so we can send ACKs
     while True:
-        packet, addr = sock.recvfrom(576)
-        source_port, dest_port, seqnum, acknum,\
-        final, window_size, checksum, contents = unpack_packet(packet)
 
-        recv_file.write(contents)
+        # Receive every other packet
+        packet, addr = recv_sock.recvfrom(576)
 
-        log = str(datetime.datetime.now()) + " " + str(source_port) + " " + str(dest_port) + " " + str(seqnum) + " " + str(acknum)
+        source_port, dest_port, seqnum, acknum, header_length, \
+            ack, final, window_size, contents = util.unpack(packet)
+
+
+        checksum = util.get_checksum(packet)
+
+        log = str(datetime.datetime.now()) + " " + str(source_port) + " " + str(dest_port) + " " + str(
+            seqnum) + " " + str(acknum)
         if final:
             log += " FIN"
         log_file.write(log + "\n")
 
-        ack_segment = make_ack(seqnum)
+        # ACK the packet if it's uncorrupted; otherwise send NAK.
+        packet_valid = checksum == 0 and next_acknum == acknum
+
+        if packet_valid:
+            recv_file.write(contents)
+            next_acknum += 1
+
+        ack_segment = util.make_packet(out_port, sender_port,
+                                        seqnum, acknum, packet_valid,
+                                        final, 1, "")
         ack_sock.send(ack_segment)
-        if final == 1:
+        if final:
             break
--- Sender.py
+++ Sender.py
@@ -2,100 +2,33 @@
 import datetime
 import signal
 import socket
-import struct
 import sys
-HEADER_SIZE = 20
+import util
 
-
-def unpack_ack(segment):
-    header = bytearray(segment[:20])
-    packet_source_port = int(header[0]) * 256 + int(header[1])
-    packet_dest_port = int(header[2]) * 256 + int(header[3])
-    packet_seqnum = 256 * 256 * 256 * int(header[4]) +\
-                    256 * 256 * int(header[5]) +\
-                    256 * int(header[6]) +\
-                    int(header[7])
-    packet_acknum = 256 * 256 * 256 * int(header[8]) +\
-                    256 * 256 * int(header[9]) +\
-                    256 * int(header[10]) +\
-                    int(header[11])
-    packet_final = int(header[13]) - 16
-    packet_window_size = 256 * int(header[14]) + int(header[15])
-    packet_checksum = 256 * int(header[16]) + int(header[17])
-    return packet_source_port, packet_dest_port,\
-           packet_seqnum, packet_acknum, packet_final,\
-           packet_window_size, packet_checksum
-
-
-def make_packet(contents, final):
-    header = bytearray(HEADER_SIZE)
-
-    header[0] = ack_port / 256 # first two bytes are source port
-    header[1] = ack_port % 256
-
-    header[2] = remote_port / 256 # second two bytes are destination port
-    header[3] = remote_port % 256
-
-    header[4] = seqnum / 256 / 256 / 256 # next four bytes are sequence number
-    header[5] = seqnum / 256 / 256
-    header[6] = seqnum / 256
-    header[7] = seqnum % 256
-
-    header[8] = acknum / 256 / 256 / 256 # next four bytes are ack number
-    header[9] = acknum / 256 / 256
-    header[10] = acknum / 256
-    header[11] = acknum % 256
-
-    header[12] = HEADER_SIZE/4 * 16  # header size is always 5 words in our case
-                                    # multiply by 16 to leave unused bytes blank
-
-    # header[13] is 2 empty bits + the flag field
-    # In the sender, it only depends on FIN because the others aren't
-    # implemented and it never sends ACKs
-    if final:
-        header[13] = 1
-    else:
-        header[13] = 0
-
-    header[14] = window_size / 256
-    header[15] = window_size % 256
-
-    header[16] = checksum / 256
-    header[17] = checksum % 256
-
-    header[18] = 0
-    header[19] = 0
-
-    return header + contents
-
-
-def shutdown(signum, frame):
-    send_sock.close()
-    ack_sock.close()
-    exit(0)
-
+WINDOW_SIZE = 1
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGINT, util.shutdown)
 
+    # Process command line args
     try:
         filename = sys.argv[1]
         remote_ip = socket.gethostbyname(sys.argv[2])
         remote_port = int(sys.argv[3])
         ack_port = int(sys.argv[4])
         log_filename = sys.argv[5]
-        try:
-            window_size = int(sys.argv[6])
-        except IndexError:
-            window_size = 1
-    except IndexError, TypeError:
-        exit('usage: ./sender.py <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size>')
 
+    except IndexError, TypeError:
+        exit('usage: ./sender.py <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename>')
+
+    # Create sockets
     try:
+        # UDP socket for packets
         send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         send_sock.bind(("", ack_port))
 
+        # TCP socket for acks
         ack_sock = socket.socket()
         ack_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         ack_sock.bind(("", ack_port))
@@ -103,50 +36,80 @@ if __name__ == '__main__':
     except socket.error:
         exit('Error creating socket.')
 
+    # Initialize acknum and seqnum to 0
     seqnum = 0
     acknum = 0
-    last_packet = False
+    final = False # Boolean indicating that this is the last packet
 
+    # Open files to reading and writing
     send_file = open(filename)
     if log_filename == "stdout":
         log_file = sys.stdout
     else:
         log_file = open(log_filename, 'w')
 
-    text = send_file.read(556) # 576 - TCP header
-    checksum = 0
-    if text == "":
-        last_packet = True
-    packet = make_packet(text, last_packet)
-    send_sock.sendto(packet, (remote_ip, remote_port))
+    tcp_established = False
+    text = send_file.read(556)  # 576 - TCP header
 
-    recv_sock, addr = ack_sock.accept()
+    while(not tcp_established):
+        try:
+            packet = util.make_packet(ack_port, remote_port, seqnum, acknum, False, False, WINDOW_SIZE, text)
+            send_sock.sendto(packet, (remote_ip, remote_port))
+
+            signal.signal(signal.SIGALRM, util.timeout)
+            signal.alarm(1)
+
+            recv_sock, addr = ack_sock.accept()
+            tcp_established = True
+            recv_sock.settimeout(1)
+        except socket.timeout:
+            continue
 
     while True:
-        ack = recv_sock.recv(20)
-        source_port, dest_port, ack_seqnum, ack_acknum, final,\
-            window_size, checksum = unpack_ack(ack)
+        try:
+            ack = recv_sock.recv(20)
+            ack_source_port, ack_dest_port, ack_seqnum, ack_acknum, ack_header_length, \
+                ack_valid, ack_final, ack_window_size, ack_contents = util.unpack(ack)
 
-        log = str(datetime.datetime.now()) + " " + str(source_port) + " " + str(dest_port) + " " + str(ack_seqnum) + " " + str(ack_acknum) + " " + "ACK"
+            log = str(datetime.datetime.now()) + " " + \
+                  str(ack_source_port) + " " + \
+                  str(ack_dest_port) + " " + \
+                  str(ack_seqnum) + " " + \
+                  str(ack_acknum)
 
-        if final == 1:
-            log_file.write(log + " FIN\n")
-            break
+            if ack_valid:
+                log = log + " ACK"
+            if ack_final:
+                log_file.write(log + " FIN\n")
+                break
 
-        log_file.write(log + "\n")
+            log_file.write(log + "\n")
 
-        if ack_acknum == seqnum:
+            if ack_acknum == acknum and ack_valid:
 
-            text = send_file.read(556) # 576 - TCP header
-            checksum = 0
+                text = send_file.read(556)  # 576 - TCP header
 
-            if text == "":
-                last_packet = True
+                if text == "":
+                    final = True
 
-            seqnum += 1
-            acknum += 1
+                seqnum += 1
+                acknum += 1
 
-            packet = make_packet(text, last_packet)
+                packet = util.make_packet(ack_port, remote_port,
+                                          seqnum, acknum, False,
+                                          final, WINDOW_SIZE,
+                                          text)
+
+                send_sock.sendto(packet, (remote_ip, remote_port))
+
+            else:
+                raise socket.timeout
+
+        except socket.timeout:
+            packet = util.make_packet(ack_port, remote_port,
+                                      seqnum, acknum, False,
+                                      final, WINDOW_SIZE,
+                                      text)
 
             send_sock.sendto(packet, (remote_ip, remote_port))
 
